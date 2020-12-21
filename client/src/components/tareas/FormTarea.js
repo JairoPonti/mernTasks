f990f0626776f92d5c16bf3154e8e0ca75291ef8
jairoPonti <jairmponti@gmail.com>
2020-12-21 16:55:13 -0300

    Avance en creación de tareas

--- client/src/components/tareas/FormTarea.js
+++ client/src/components/tareas/FormTarea.js
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
+import { AGREGAR_TAREA } from '../../types';
 
 const FormTarea = () => {
 
-   //Extraersi un proyecto está activo
+   //Extraer si un proyecto está activo
    const proyectosContext = useContext(proyectoContext)
    const { proyecto } = proyectosContext
+
+     //Obtener la función del context de tarea
+     const tareasContext = useContext(tareaContext);
+     const { agregarTarea } = tareasContext;
+ 
+   // State del formulario
+   const [tarea, guardarTarea] = useState({
+      nombre: ''
+
+   })
+
+   // Extraer el nombre del proyecto
+   const { nombre } = tarea;
 
       // Si no hay proyecto seleccionado
       if(!proyecto) return null;
@@ -13,15 +28,42 @@ const FormTarea = () => {
         // Array destructuring para extraer el proyecto actual
         const [proyectoActual] = proyecto;
 
+        //Leer los valores del formulario
+        const handleChange = e => {
+          guardarTarea({
+            ...tarea,
+            [e.target.name] : e.target.value
+          })
+        }
+
+        const onSubmit = e => {
+          e.preventDefault();
+
+          //Validar
+
+          //Pasar la validación 
+
+          //Agregar la nueva tarea al state de tareas
+          tarea.proyectoId = proyectoActual.id;
+          tarea.estado = false;
+          agregarTarea(tarea);
+
+          // Reiniciar el form
+        }
+
     return (
         <div className="formulario">
-       <form>
+       <form
+         onSubmit={onSubmit}
+       >
          <div className="contenedor-input">
              <input
                type="text"
                className="input-text"
                placeholder="Nombre Tarea..."
                name="nombre"
+               value={nombre}
+               onChange={handleChange}
              />
          </div>
 
